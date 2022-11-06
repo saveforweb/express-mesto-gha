c09cf0af5a147e69045ea2715decc8cb42966d30
Anton Popov <anton@bbbro.ru>
2022-11-07 00:32:41 +0300

    fix: after tests

--- controllers/users.js
+++ controllers/users.js
@@ -8,10 +8,16 @@ module.exports.getUsers = (req, res) => {
 
 module.exports.getUser = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send({ data: user }))
+    .then((user) => {
+      if (!user === null) {
+        res.send({ data: user });
+      } else {
+        res.status(404).send({ message: 'Пользователь не найден.' });
+      }
+    })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(404).send({ message: 'Пользователь не найден.' });
+        res.status(400).send({ message: 'Пользователь не найден.' });
       } else {
         res.status(500).send({ message: err.message });
       }
