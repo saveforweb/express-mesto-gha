b6486ac4f14c03d009289d6fb073507acb1bcd29
Anton Popov <anton@bbbro.ru>
2022-11-07 17:20:29 +0300

    fix: after tests

--- controllers/users.js
+++ controllers/users.js
@@ -18,7 +18,7 @@ module.exports.getUser = (req, res) => {
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        res.status(errorCodes.notFound).send({ message: 'Переданы некорректные данные.' });
+        res.status(errorCodes.badRequest).send({ message: 'Переданы некорректные данные.' });
       } else {
         res.status(errorCodes.internalServerError).send({ message: err.message });
       }
@@ -32,7 +32,7 @@ module.exports.createUser = (req, res) => {
     .then((user) => res.send({ data: user }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        res.status(errorCodes.notFound).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+        res.status(errorCodes.badRequest).send({ message: 'Переданы некорректные данные при создании пользователя.' });
       } else {
         res.status(errorCodes.internalServerError).send({ message: err.name });
       }
@@ -52,7 +52,7 @@ module.exports.updateInfoUser = (req, res) => {
       if (err.name === 'CastError') {
         res.status(errorCodes.notFound).send({ message: 'Пользователь не найден.' });
       } else if (err.name === 'ValidationError') {
-        res.status(errorCodes.notFound).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+        res.status(errorCodes.badRequest).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
       } else {
         res.status(errorCodes.internalServerError).send({ message: err.name });
       }
