ec2f38ad410682fce073e27dacea708a6241c1a8
Anton Popov <anton@bbbro.ru>
2022-11-23 00:32:34 +0300

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { celebrate, Joi, Segments } = require('celebrate');
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getUsers, getUser, updateInfoUser, updateAvatarUser, getCurrentUser,
@@ -8,16 +8,11 @@ const {
 router.get('/', getUsers);
 router.get('/me', getCurrentUser);
 
-router.get('/:userId', celebrate(
-  {
-    [Segments.PARAMS]: Joi.object({
-      id: Joi.alternatives().try(
-        Joi.string().equal('me'),
-        Joi.string().hex().length(24),
-      ).required(),
-    }).required(),
-  },
-), getUser);
+router.get('/:userId', celebrate({
+  params: Joi.object().keys({
+    userId: Joi.string().hex().length(24),
+  }).required(),
+}), getUser);
 
 router.patch('/me', updateInfoUser);
 router.patch('/me/avatar', updateAvatarUser);
