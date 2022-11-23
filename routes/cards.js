6db99b612623563a112fa1958bf5090e0f65fd89
Anton Popov <anton@bbbro.ru>
2022-11-23 11:44:07 +0300

    fix

--- routes/cards.js
+++ routes/cards.js
@@ -1,11 +1,19 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
 router.get('/', getCards);
-router.post('/', createCard);
+
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30).required(),
+    link: Joi.string().uri({ scheme: ['http', 'https'] }).required(),
+  }),
+}), createCard);
+
 router.delete('/:cardId', deleteCard);
 router.put('/:cardId/likes', likeCard);
 router.delete('/:cardId/likes', dislikeCard);
