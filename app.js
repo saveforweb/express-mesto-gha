9bd1d354d54f185c68931c9c5e30baa18e522d84
Anton Popov <anton@bbbro.ru>
2022-11-27 21:15:56 +0300

    fix

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
-    avatar: Joi.string().regex(regexList.urlRegex).uri({ scheme: ['http', 'https'] }).required(),
+    avatar: Joi.string().regex(regexList.urlRegex).uri({ scheme: ['http', 'https'] }),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   }),
